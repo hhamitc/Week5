fc275421890caca15773c900775adcc717ed7308
Hamit Canpolat <hhamitc@gmail.com>
2024-10-29 01:56:38 +0300

    Liste örneği yapıldı.

---
+++ Week5_02_ListsPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        List<string> Visitors = new List<string>();//Visitors adlı listeyi oluşturuyoruz.
+
+
+        //Listeye elemanları ekliyoruzç
+        Visitors.Add("Şebnem Ferah");
+        Visitors.Add("Bülent Ersoy");
+        Visitors.Add("Ajda Pekkan");
+        Visitors.Add("Ebru Gündeş");
+        Visitors.Add("Hadise");
+        Visitors.Add("Hande Yener");
+        Visitors.Add("Tarkan");
+        Visitors.Add("Funda Arar");
+        Visitors.Add("Demet Akalın");
+
+        Console.WriteLine("----- ZİYARETÇİLER ------");// Lİste öncesi başlık ekleme
+
+        //Tüm ziyaretçilerin ekrana yazdırılması.
+        foreach (var visitor in Visitors)
+        {
+            Console.WriteLine(visitor);
+        }
+
+
+        Console.ReadLine();
+    }
+}
