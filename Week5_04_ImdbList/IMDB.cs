60c78f1d0bdad81b8e5cec297db55547a2063ba3
Hamit Canpolat <hhamitc@gmail.com>
2024-10-29 23:40:51 +0300

    Yorum satırları eklendi.

--- Week5_04_ImdbList/IMDB.cs
+++ Week5_04_ImdbList/IMDB.cs
@@ -2,10 +2,12 @@
 {
     public class Movie
     {
+        //Movie sınıfının Propertyleri tanımlandı
         public string Name { get; set; }
         public double Rating { get; set; }
 
 
+        //Constructor oluşturuldu
         public Movie(string name, double rating)
         {
             Name = name;
--- Week5_04_ImdbList/Program.cs
+++ Week5_04_ImdbList/Program.cs
@@ -4,48 +4,52 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        // Film listesini tutmak için bir liste oluşturuluyor
         List<Movie> MovieList = new List<Movie>();
 
         while (true)
         {
+            // Ekranı temizle
             Console.Clear();
             Console.WriteLine("Eklemek İstediğiniz Film adını giriniz:  ");
 
+            // Kullanıcıdan film adını al
             string name = Console.ReadLine();
 
             double rating;
             Console.Write("Puanını giriniz: ");
 
-
+            // Kullanıcıdan geçerli bir puan alana kadar döngü devam eder
             while (!double.TryParse(Console.ReadLine(), out rating) || !(rating >= 0 && rating <= 10))
             {
                 Console.WriteLine("Geçerli bir puan giriniz.");
             }
 
-
+            // Yeni bir film nesnesi oluştur ve listeye ekle
             Movie movie = new Movie(name, rating);
             MovieList.Add(movie);
 
             Console.WriteLine("Başarıyla Eklendi.");
-
             Console.WriteLine("-----------------------------------");
 
             Console.WriteLine("Yeni bir film eklemek için 1 çıkmak için 2 değerini girin.");
 
             int addNew;
 
+            // Kullanıcıdan geçerli bir seçim alana kadar döngü devam eder
             while (!int.TryParse(Console.ReadLine(), out addNew) || !(addNew == 1 || addNew == 2))
             {
                 Console.WriteLine("Hatalı giriş!! \nYeni bir film eklemek için 1 çıkmak için 2 değerini girin.");
             }
 
+            // Kullanıcı 1 seçerse döngü devam eder, 2 seçerse program sonlanır ve listeler gösterilir
             if (addNew == 1)
             {
                 continue;
             }
             else if (addNew == 2)
             {
-
+                // Ekranı temizle ve listeleri göster
                 Console.Clear();
                 DisplayFullList();
                 Console.WriteLine("-------------------");
@@ -55,9 +59,9 @@ public class Program
                 Console.ReadKey();
                 return;
             }
-
         }
 
+        // Tüm film listesini ekrana yazdırır
         void DisplayFullList()
         {
             Console.WriteLine("----- Filmler Tam Liste -----");
@@ -67,7 +71,7 @@ public class Program
             }
         }
 
-
+        // Belirtilen puan aralığındaki filmleri ekrana yazdırır
         void DisplayListInRangeOf(double floorRating, double cealingRating)
         {
             Console.WriteLine($"----- Puanı {floorRating} - {cealingRating} Arasındaki Filmler  -----");
@@ -77,10 +81,10 @@ public class Program
                 {
                     Console.WriteLine($"Film Adı: {movie.Name}, IMDB Puanı: {movie.Rating}");
                 }
-
             }
         }
 
+        // Belirtilen harf ile başlayan filmleri ekrana yazdırır
         void DisplayListStartsBy(string letter)
         {
             Console.WriteLine($"----- İsmi {letter} ile başlayan Filmler  -----");
@@ -90,11 +94,8 @@ public class Program
                 {
                     Console.WriteLine($"Film Adı: {movie.Name}, IMDB Puanı: {movie.Rating}");
                 }
-
             }
         }
-
-
 
         Console.ReadKey();
     }
