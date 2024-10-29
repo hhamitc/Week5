6aafc691f4d3015baf627ec1978efe187623f107
Hamit Canpolat <hhamitc@gmail.com>
2024-10-30 02:08:50 +0300

    Try Catch ödevi yapıldı readme eklendi.

---
+++ Week5_05_TryCatchPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            // Ekranı temizle
+            Console.Clear();
+            try
+            {
+                // Kullanıcıdan bir sayı girmesini iste
+                Console.WriteLine("Lütfen bir sayı girin. (Saçmalamak serbest)");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                // Girilen sayının karesini hesapla
+                int karesi = (int)Math.Pow(sayi, 2);
+
+                // Sonucu ekrana yazdır
+                Console.WriteLine(karesi);
+                break; // Başarılı giriş yapıldığında döngüden çık
+
+            }
+            catch (Exception ex)
+            {
+                // Hatalı giriş yapıldığında hata mesajı göster
+                Console.WriteLine("Hatalı bir giriş yaptınız.");
+                Console.ReadKey(); // Kullanıcının bir tuşa basmasını bekle
+            }
+        }
+
+        // Başarılı giriş yapıldığında ekrana mesaj yazdır
+        Console.WriteLine("Başarılı.");
+
+        // Programın sonlanmasını bekle
+        Console.ReadLine();
+    }
+}
---
+++ Week5_05_TryCatchPractice/README.md
@@ -0,0 +1,14 @@
+﻿# Patika+ BackEnd Web Developer Eğitimi 5. Hafta Try-Catch Pratik Ödevi
+
+Bu pratik, aşağıdaki adımları gerçekleştirerek basit bir **try-catch** uygulaması yapmayı amaçlar:
+
+## İsterler
+
+1. **Kullanıcıdan bir sayı girişi isteyin:**  
+   - Kullanıcıdan bir sayı girmesini isteyen bir program yazın.
+
+2. **Geçerli giriş:**  
+   - Kullanıcı geçerli bir sayı girdiğinde, bu sayının karesini ekrana yazdırın.
+
+3. **Geçersiz giriş:**  
+   - Kullanıcı geçersiz bir giriş yaparsa (örneğin sayı yerine harf veya sembol girdiğinde), "Geçersiz giriş! Lütfen bir sayı giriniz." şeklinde bir hata mesajı gösterin.
